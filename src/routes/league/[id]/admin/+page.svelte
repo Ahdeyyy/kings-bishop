f3fc8c0b693a478dd0acf1b3fa7ad6fee8d7ee20
Ahdeyyy <85193076+Ahdeyyy@users.noreply.github.com>
2024-12-25 19:16:12 +0100

    feat: add player management and fixture components to league admin page

---
+++ src/routes/league/[id]/admin/+page.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	import { Button, buttonVariants } from '@/components/ui/button';
+	import type { PageData } from './$types';
+	import * as AlertDialog from '$lib/components/ui/alert-dialog';
+	// import Fixture from './Fixture.svelte';
+
+	let { data }: { data: PageData } = $props();
+</script>
+
+{#snippet player()}
+	<article class="flex items-center gap-4">
+		<p>name</p>
+		<p>played</p>
+		<p>won</p>
+		<p>drawn</p>
+		<p>lost</p>
+		<p>points</p>
+		<p>sb points</p>
+		<div>
+			<AlertDialog.Root>
+				<AlertDialog.Trigger class={buttonVariants({ variant: 'destructive' })}
+					>Remove</AlertDialog.Trigger
+				>
+				<AlertDialog.Content>
+					<AlertDialog.Header>
+						<AlertDialog.Title>Are you absolutely sure?</AlertDialog.Title>
+						<AlertDialog.Description>
+							This action cannot be undone. This will remove user from the league.
+						</AlertDialog.Description>
+					</AlertDialog.Header>
+					<AlertDialog.Footer>
+						<AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
+						<AlertDialog.Action class={buttonVariants({ variant: 'destructive' })}
+							>Remove</AlertDialog.Action
+						>
+					</AlertDialog.Footer>
+				</AlertDialog.Content>
+			</AlertDialog.Root>
+		</div>
+	</article>
+{/snippet}
+
+<section>
+	<h2 class="text-center text-lg">Players</h2>
+	<ul>
+		<li>{@render player()}</li>
+	</ul>
+</section>
---
+++ src/routes/league/[id]/admin/Fixture.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import { Button } from '@/components/ui/button';
+	import { Separator } from '@/components/ui/separator';
+	import { LinkIcon } from 'lucide-svelte';
+	type result = 'w' | 'b' | 't';
+	type FixtureProps = {
+		whiteUsername: string;
+		blackUsername: string;
+		matchUrl: string;
+		result: result;
+	};
+</script>
+
+<article
+	class="flex w-fit items-center gap-4 rounded-md px-3 py-1 shadow-md ring-1 ring-secondary md:w-full md:justify-between"
+>
+	<div class="flex md:w-1/3 md:justify-between md:gap-6">
+		<div class="flex items-center gap-4">
+			<div class="size-4 rounded bg-white shadow-md ring-2 ring-secondary"></div>
+			<p>white</p>
+			<p>1/2</p>
+			<Separator orientation="vertical" />
+			<p>1/2</p>
+			<p>black</p>
+			<div class="size-4 rounded bg-black shadow-md ring-2 ring-secondary"></div>
+		</div>
+	</div>
+	<div class="flex items-center gap-2">
+		<LinkIcon size={16} />
+		<Button variant="link">url to match</Button>
+	</div>
+	<!-- <div class="flex items-center gap-4">
+		<p class="max-w-24 text-balance">-</p>
+		<Button variant="default">Arbitrate</Button>
+	</div> -->
+</article>
